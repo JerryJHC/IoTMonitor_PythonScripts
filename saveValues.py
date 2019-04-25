0bdf07b84593cd6ea00207c42570b6c77997965e
Jerry Henriquez <jerryhenriquez@gmail.com>
2019-04-25 18:14:28 +0200

    modificado script para no enviar datetime, unicamente los valores, para efectos de log se impreme datetime

--- saveValues.py
+++ saveValues.py
@@ -1,16 +1,7 @@
 # importing the requests library 
 import requests
-import json
 from sense_hat import SenseHat
 from datetime import datetime
-
-# Convert datetime to json serializable
-class DateTimeEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, datetime):
-            return o.isoformat()
-
-        return json.JSONEncoder.default(self, o)
 
 sense = SenseHat()
 
@@ -20,15 +11,15 @@ API_ENDPOINT = "https://us-central1-iotmonitor-9c57a.cloudfunctions.net/save"
 # data to be sent to firebase
 data = {"temperature": sense.get_temperature(), 
 		"humidity": sense.get_humidity(), 
-		"pressure": sense.get_pressure(), 
-		"datetime": datetime.now()} 
+		"pressure": sense.get_pressure()} 
 
 # sending post request and saving response as response object 
-r = requests.post(url = API_ENDPOINT, data=json.dumps(data, cls=DateTimeEncoder), headers={'Content-Type': 'application/json'})
+r = requests.post(url = API_ENDPOINT, data=data)
 
 # extracting response text 
 pastebin_url = r.text 
 print("The pastebin URL is:%s"%pastebin_url) 
+print("Time: %s"%str(datetime.now()))
 print("Saved values: ")
 print(data)
 print("------------------------------------------------")
