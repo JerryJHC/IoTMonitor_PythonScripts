143759d716c4a1ddebfe6c5149a1e7ceed725cc3
Jerry Henriquez <jerryhenriquez@gmail.com>
2019-04-27 16:02:44 +0200

    modificado valor de temperatura que se envia para calcularlo restando la diferencia con la temperatura de cpu para obtener un valor mas exacto

--- saveValues.py
+++ saveValues.py
@@ -1,5 +1,6 @@
 # importing the requests library 
 import requests
+import subprocess
 from sense_hat import SenseHat
 from datetime import datetime
 
@@ -8,8 +9,18 @@ sense = SenseHat()
 # defining the firebase-endpoint 
 API_ENDPOINT = "https://us-central1-iotmonitor-9c57a.cloudfunctions.net/save"
 
+#CPU Heat
+cpu = subprocess.check_output("cat /sys/class/thermal/thermal_zone0/temp", shell=True)
+cpu = int(cpu)/1000
+
+#Average temperature
+avgTemperature = (sense.get_temperature_from_humidity() + sense.get_temperature_from_pressure()) / 2
+
+#Calculate temperature
+temperature = avgTemperature - ( cpu - avgTemperature )
+
 # data to be sent to firebase
-data = {"temperature": sense.get_temperature(), 
+data = {"temperature": temperature, 
 		"humidity": sense.get_humidity(), 
 		"pressure": sense.get_pressure()} 
 
